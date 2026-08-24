4b0ff87fb432df846e327a9cb7a8e91d7fe27e32
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-08-24 19:22:40 +0400

    test: revert CI validation scaffolding

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -3,7 +3,6 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  controllerTest;
   constructor(private readonly appService: AppService) {}
 
   @Get()
--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  test = 'test';
   getData(): { message: string } {
     return { message: 'Hello API' };
   }

9d249e8220540cc9dd8213cb03d5a0ab5f1f1e0e
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-08-24 19:07:55 +0400

    ci: add test variables to check linting

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  test: string = 'test';
   getData(): { message: string } {
     return { message: 'Hello API' };
   }

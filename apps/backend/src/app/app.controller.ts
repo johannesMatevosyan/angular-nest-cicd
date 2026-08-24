2d3a3911cf6c4558acfa987b2548443b9e4c4aa0
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-08-24 19:15:27 +0400

    ci: modify test variables to check linting

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  controllerTest;
+  test = 'test';
   constructor(private readonly appService: AppService) {}
 
   @Get()

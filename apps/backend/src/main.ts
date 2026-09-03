41191a58fc005ab54fc397feadbe1c9c9cb2e6ae
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-09-03 22:41:48 +0400

    feat: streamline CORS setup in main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,17 +11,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
-
-  console.log(`🚀 CORS enabled for: ${process.env.FRONTEND_URL}`);
-
-  app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
-  });
 }
 
 bootstrap();

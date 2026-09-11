77cbe5c40a3aa5164fbe32b049ec3d085f3981ff
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-09-11 13:53:28 +0400

    Revert "feat: test CI failure"
    This reverts commit 3eea55c75c349e3e4d7e7bce0df2992bc36e8dd2.

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
-export const appConfig: Application = {
+export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes),
